7303f4162a895821ab33d4c43f0f9c24cfa7b6eb
raminpz <raminp.1012@gmail.com>
2022-08-01 12:51:05 -0500

    Initial commit

---
+++ src/Variables/TipoDatos.java
@@ -0,0 +1,26 @@
+package Variables;
+
+public class TipoDatos {
+    public static void main(String[] args) {
+        /***
+         * Variables en Java
+         */
+
+        int edad = 30;
+        System.out.println("Tipo de dato int: " + edad);
+
+        long dni = 44106817;
+        System.out.println("Tipo de dato long: " + dni);
+
+        double precio = 1998.99;
+        System.out.println("Tipo de dato double: " + precio);
+
+        boolean esCasado= false;
+        System.out.println("Tipo de dato boolean: " + esCasado);
+
+        String nombre = "Ramiro";
+        System.out.println("Tipo de dato String = " + nombre);
+
+
+    }
+}
